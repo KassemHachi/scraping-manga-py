8dd68a348d7820a0d8efa37d6af8c53b37cc0d2c
KassemHachi <kassem.hachi@outlook.com>
2020-11-22 23:05:15 +0100

    update scraping-manga.py file

--- scraping manga.py
+++ scraping manga.py
@@ -3,13 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 import os
-url = input('please get your chapiter manga frome https://3asq.org/ : ')
-page = requests.get(url)
+url = input('please get your chapiter manga from https://3asq.org/ : ')
+dirname = input('please entre path to save your manga :')
 print('downloading manga chapiter ...')
+page = requests.get(url)
 soup = BeautifulSoup(page.content ,"html.parser")
 title = soup.find("head").find("title").text
 pagee =  soup.find_all("img", {"class":"wp-manga-chapter-img"})
-dirname = input('please entre path to save your manga :')
 for i in pagee:
   urllib.request.urlretrieve(i['src'],dirname + '/'+ i['id']+'.jpg')
 
