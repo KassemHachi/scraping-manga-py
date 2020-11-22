ba2eb19ca9c8cf8f65ef48806dc23d73f290a403
KassemHachi <kassem.hachi@outlook.com>
2020-11-22 23:01:41 +0100

    remplce url and direction to input

--- scraping manga.py
+++ scraping manga.py
@@ -2,12 +2,15 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
-
-page = requests.get("https://3asq.org/manga/dr-stone/163/")
+import os
+url = input('please get your chapiter manga frome https://3asq.org/ : ')
+page = requests.get(url)
+print('downloading manga chapiter ...')
 soup = BeautifulSoup(page.content ,"html.parser")
-
+title = soup.find("head").find("title").text
 pagee =  soup.find_all("img", {"class":"wp-manga-chapter-img"})
+dirname = input('please entre path to save your manga :')
 for i in pagee:
-  urllib.request.urlretrieve(i['src'],i['id']+'.jpg')
+  urllib.request.urlretrieve(i['src'],dirname + '/'+ i['id']+'.jpg')
 
-print('downloaded images')
+print('downloaded manga')
